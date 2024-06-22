008a5c585395ed44723dd77c15889127f6372334
loki <131438944+logesh-jarvis@users.noreply.github.com>
2024-06-22 15:13:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-curl -O /home/coder/project/GitHub.copilot-1.206.0.vsix https://github.com/logesh-jarvis/testdummy/blob/main/GitHub.copilot-1.206.0.vsix && code-server --install-extension /home/coder/project/GitHub.copilot-1.206.0.vsix
+curl -o /home/coder/project/GitHub.copilot-1.206.0.vsix https://github.com/logesh-jarvis/testdummy/blob/main/GitHub.copilot-1.206.0.vsix
+code-server --install-extension /home/coder/project/GitHub.copilot-1.206.0.vsix
